1855abdd042707052466f47c1cc3ba67bd79a25d
Silent K <silentksionda@gmail.com>
2014-12-28 18:16:46 -0600

    Updating timer functionality
    Pause/start timer works with space or enter. Timer runs even when
    computer is idle/sleeping.
    
    Code refactored to have less global variables and be more modular.

--- timer.js
+++ timer.js
@@ -1,8 +1,5 @@
-// TODO: Take jQuery out
-// It's a thing I assumed I needed just cause, but I don't think I do
-// Don't add libraries unless necessary!
 
-function zero_fill(length, number) {
+function zero_fill (length, number) {
     var number_string = '' + number
     while (number_string.length < length) {
         number_string = '0' + number_string
@@ -10,11 +7,11 @@ function zero_fill(length, number) {
     return number_string
 }
 
-function two_digit(number) {
+function two_digit (number) {
     return zero_fill(2,number)
 }
 
-function timerParse(input) {
+function timerParse (input) {
     var parse = zero_fill(6,input)
 
     return {
@@ -24,7 +21,83 @@ function timerParse(input) {
     }
 }
 
-function audioAssets() {
+function hhmmssToSeconds (hhmmss) {
+    var timer = timerParse(hhmmss)
+    seconds = timer.seconds
+    seconds += timer.minutes*60
+    seconds += timer.hours*3600
+
+    return seconds
+}
+
+function currentTime () {
+    return Math.floor(new Date().getTime()/1000)
+}
+
+function displayTimer (seconds) {
+    var htmlHours = two_digit(Math.floor(seconds/3600))
+    document.getElementById('hours').innerHTML = htmlHours
+    document.title = htmlHours + ':'
+
+    var htmlMinutes = two_digit(Math.floor((seconds%3600)/60))
+    document.getElementById('minutes').innerHTML = htmlMinutes
+    document.title += htmlMinutes + ':'
+
+    var htmlSeconds = two_digit(seconds%60)
+    document.getElementById('seconds').innerHTML = htmlSeconds
+    document.title += htmlSeconds
+}
+
+function countdown (expiresAt) {
+    var updateOnSecond = function () {
+        var seconds = expiresAt - Math.floor(new Date().getTime()/1000)
+
+        if (seconds < 0) {
+            seconds = 0
+        }
+
+        alarm.play(seconds)
+        displayTimer(seconds)
+    }
+
+    return updateOnSecond
+}
+
+function stopTimer (timer) {
+    if (timer.countdownID !== -1) {
+        alarm.stop()
+        clearInterval(timer.countdownID)
+        timer.countdownID = -1
+    }
+}
+
+function startOrPause (timer) {
+    if (timer.input) {
+        timer.remaining = hhmmssToSeconds(timer.input)
+        timer.expiresAt = -1
+        timer.input = ''
+    }
+
+    if (timer.expiresAt != -1) {
+        timer.remaining = timer.expiresAt - currentTime()
+        timer.expiresAt = -1
+
+        stopTimer(timer)
+    }
+    else if (timer.remaining != -1) {
+        timer.expiresAt = currentTime() + timer.remaining
+        timer.remaining = -1
+        timer.countdownID = setInterval(countdown(timer.expiresAt), 1000)
+    }
+}
+
+function timerInput (timer, keyInput) {
+    stopTimer(timer)
+    timer.input += keyInput
+    displayTimer(hhmmssToSeconds(timer.input))
+}
+
+function audioAssets () {
     var audio = {
         allemande: new Audio('assets/Allemande.ogg'),
         tenMinutes: new Audio('assets/10m_remaining.ogg'),
@@ -41,66 +114,30 @@ function audioAssets() {
     return audio
 }
 
-window.onload = function () {
+function setupAlarm () {
     var audio = audioAssets()
 
-    var seconds = 0
-
-    var input = '500'
-
-    var updateTimer = function () {
-        if (input) {
-            var timer = timerParse(input)
-
-            var htmlHours = two_digit(timer.hours)
-            document.getElementById('hours').innerHTML = htmlHours
-            document.title = htmlHours + ':'
-
-            var htmlMinutes = two_digit(timer.minutes)
-            document.getElementById('minutes').innerHTML = htmlMinutes
-            document.title += htmlMinutes + ':'
-
-            var htmlSeconds = two_digit(timer.seconds)
-            document.getElementById('seconds').innerHTML = htmlSeconds
-            document.title += htmlSeconds
-        }
-
-        else {
-            var htmlHours = two_digit(Math.floor(seconds/3600))
-            document.getElementById('hours').innerHTML = htmlHours
-            document.title = htmlHours + ':'
-
-            var htmlMinutes = two_digit(Math.floor((seconds%3600)/60))
-            document.getElementById('minutes').innerHTML = htmlMinutes
-            document.title += htmlMinutes + ':'
-
-            var htmlSeconds = two_digit(seconds%60)
-            document.getElementById('seconds').innerHTML = htmlSeconds
-            document.title += htmlSeconds
-        }
-    }
-
-    setInterval(function() {
-        if (!input) {
-            if (seconds <= 1) {
+    return {
+        play: function (seconds) {
+            if (seconds <= 0) {
                 audio.allemande.play()
-                seconds = 1
+                seconds = 0
             }
-            // TODO: only excute if audio cues turned on
             /*
-            else if (seconds === 10+1) {
+            // TODO: only excute if audio cues turned on
+            else if (seconds === 10) {
                 audio.finalCountdown.play()
             }
-            else if (seconds === 30+1) {
+            else if (seconds === 30) {
                 audio.thirtySeconds.play()
             }
-            else if (seconds === 60+1) {
+            else if (seconds === 60) {
                 audio.oneMinute.play()
             }
-            else if (seconds === 5*60+1) {
+            else if (seconds === 5*60) {
                 audio.fiveMinutes.play()
             }
-            else if (seconds === 10*60+1) {
+            else if (seconds === 10*60) {
                 audio.tenMinutes.play()
             }
             */
@@ -108,45 +145,49 @@ window.onload = function () {
                 audio.allemande.pause()
                 audio.allemande.currentTime = 0
             }
-            seconds -= 1
-        }
-        else {
+        },
+        stop: function () {
             for (var asset in audio) {
                 audio[asset].pause()
                 audio[asset].currentTime = 0
             }
         }
-        updateTimer()
-    }, 1000)
+    }
+}
 
-    document.addEventListener("keydown", function (e) {
-        //console.log(e.keyCode)
+var alarm = setupAlarm()
 
-        // Enter key has been pressed, start countdown timer
-        if (e.keyCode === 13) {
-            var timer = timerParse(input)
-            seconds = timer.seconds
-            seconds += timer.minutes*60
-            seconds += timer.hours*3600
-
-            input = ''
+window.onload = function () {
+    
+    // Timer functionality
+    var timer = {
+        remaining: 5*60,
+        expiresAt: -1,
+        countdownID: -1,
+        input: ''
+    }
+    displayTimer(5*60)
+    document.addEventListener('keydown', function (e) {
+        // Enter key has been pressed, start/pause countdown timer
+        if (e.keyCode === 13 || e.keyCode === 32) {
+            startOrPause(timer)
         }
+
         // Backspace key pressed, don't go back in browser history
         else if (e.keyCode === 8) {
             e.preventDefault()
-            input = input.slice(0,input.length-1)
+            timer.input = timer.input.slice(0,timer.input.length-1)
         }
+
         // If top row number keys are pressed, start setting timer
         else if (e.keyCode >= 48 && e.keyCode <= 57) {
-            input += (e.keyCode - 48)
+            timerInput(timer, e.keyCode - 48)
         }
         // If number pad keys are pressed, start setting timer
         else if (e.keyCode >= 96 && e.keyCode <= 105) {
-            input += (e.keyCode - 96)
+            timerInput(timer, e.keyCode - 96)
         }
-
-        // TODO: Too much state manipulation, more functional programming
-        updateTimer()
     })
+
 }
 
